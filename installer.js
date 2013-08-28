68b9b93b18515fdc804ae39acfbef144b9ddfa0e
Alex Gibson <a.gibson@me.com>
2013-08-28 14:13:25 +0100

    Update styles

--- installer.js
+++ installer.js
@@ -82,12 +82,11 @@
         var bar = document.createElement('div');
         var button = document.createElement('span');
         var close = document.createElement('span');
-        var action = 'ontouchstart' in window ? 'Tap' : 'Click';
 
         bar.id = 'installer';
         button.id = 'installer-button';
         button.setAttribute('role', 'button');
-        button.innerHTML = action + ' to install this app';
+        button.innerHTML = 'Install this app';
         close.id = 'installer-close';
         close.setAttribute('role', 'button');
         close.innerHTML = 'Close';
